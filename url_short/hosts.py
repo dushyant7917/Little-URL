fa1aa9f2c2fb843233d3a2e3ef09caf7601650f5
dushyant7917 <dushyant7917@gmail.com>
2017-06-03 11:59:28 +0530

    change in host name

--- url_short/hosts.py
+++ url_short/hosts.py
@@ -2,6 +2,6 @@ from django.conf import settings
 from django_hosts import patterns, host
 
 host_patterns = patterns('',
-    host(r'www', settings.ROOT_URLCONF, name = 'www'),
-    host(r'(?!www).*', 'url_short.hostsconf.urls', name = 'wildcard'),
+    host(r'little-url-7917', settings.ROOT_URLCONF, name = 'little-url-7917'),
+    host(r'(?!little-url-7917).*', 'url_short.hostsconf.urls', name = 'wildcard'),
 )
